a0b6c248a4cbaed51a4a5d70d41bb992d404352b
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2021-11-07 18:47:19 +0700

    add a new method for reduce time

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,97 +1,116 @@
 <template>
   <div class="hello">
-   <h1>Pomodoro</h1>
-   <section>
-     <div class="timer">
-        25:00
-     </div>
-   </section>
-   <section class="button-pomodoro">
-     <button>start</button>
-     <button>stop</button>
-   </section>
-   <section>
-     <h2>Todolist</h2>
-   </section>
-   <section class="list">
-        <div>
-          <input type="text">
-        </div>
-        <div>
-          <input type="text">
-        </div>
-        <div>
-          <input type="text">
-        </div>
-        <div>
-          <input type="text">
-        </div>
-        <div>
-          <input type="text">
-        </div>
-        <div>
-          <input type="text">
-        </div>
-        <div>
-          <input type="text">
-        </div>
-        
-   </section>
-
+    <h1>Pomodoro</h1>
+    <section>
+      <div class="timer">
+        {{ isTime }}
+      </div>
+    </section>
+    <section class="button-pomodoro">
+      <button @click="handleCountdown">start</button>
+      <button>stop</button>
+    </section>
+    <section>
+      <h2>Todolist</h2>
+    </section>
+    <section class="list">
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: "Pomodoro",
+  data() {
+    return {
+      time: 10,
+    };
+  },
   props: {
     msg: String,
+  },
+  computed: {
+    isTime() {
+      return this.time;
+    },
+  },
+  created() {
+    this.initialPage();
+  },
+  methods: {
+    initialPage() {
+     
+    },
+    handleCountdown() {
+         this.time -= 1;
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1{
+h1 {
   text-align: center;
   color: white;
 }
-.timer{
+.timer {
   color: white;
   font-size: 6em;
 }
 
-section{
-  display:flex;
+section {
+  display: flex;
   justify-content: space-evenly;
 }
-section button{
-    background:none;
-    color:#ffa260;
-    border: 2px solid;
-    padding: 1em 2em;
-    font-size: 1em;
-    transition: color 0.25s, border-color 0.25s, box-shadow 0.25s, transform 0.25s;
+section button {
+  background: none;
+  color: #ffa260;
+  border: 2px solid;
+  padding: 1em 2em;
+  font-size: 1em;
+  transition: color 0.25s, border-color 0.25s, box-shadow 0.25s, transform 0.25s;
 }
 
-section button:hover{
-    border-color: #f1ff5c;
-    color:white;
-    box-shadow: 0 0.5em 0.5em -0.4em #f1ff5c;
-    transform: translateY(-0.25em);
-    cursor: pointer;
+section button:hover {
+  border-color: #f1ff5c;
+  color: white;
+  box-shadow: 0 0.5em 0.5em -0.4em #f1ff5c;
+  transform: translateY(-0.25em);
+  cursor: pointer;
 }
-.list{
-  display:flex;
+.list {
+  display: flex;
   flex-direction: column;
   text-align: center;
 }
-.list div{
-border: 2px solid orange;
-padding: 3em 20em;
-margin: 0 auto;
+.list div {
+  border: 2px solid orange;
+  padding: 3em 20em;
+  margin: 0 auto;
 }
-.list input{
-  width: 100%;
+.list input {
+  width: 20em;
 }
 </style>
