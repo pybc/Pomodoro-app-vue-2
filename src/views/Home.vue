d1f3acd350cdc5905c787ebb5f219fe00e92a61a
Piyabute Chairiboon <90825317+pybc@users.noreply.github.com>
2021-12-05 23:39:17 +0700

    add line liff
    mock init home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,27 @@ export default {
   components: {
     Pomodoro,
   },
+   async created() {
+    await this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.initialData()
+    },
+     async initialData() {
+       liff
+  .init({
+    liffId: "123456-abcedfg" // Use own liffId
+  })
+  .then(() => {
+    // Start to use liff's api
+  })
+  .catch((err) => {
+    // Error happens during initialization
+    console.log(err.code, err.message);
+  });
+    },
+  }
 };
 </script>
 <style scoped>
